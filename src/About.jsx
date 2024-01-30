4aa77ebdb062294c38e48d3be1af131505eb36b1
CHuston13 <CDHprogrammer@gmail.com>
2024-01-30 13:31:07 -0600

    new

--- src/About.jsx
+++ src/About.jsx
@@ -3,14 +3,22 @@ import React from 'react';
 
 const About = () => {
   return (
+  
+  
     <section id="about">
-       <a href="#AboutMe" id='FirstLink'>About</a>
-      <h2>About Me</h2>
-      <p id='AboutMe'>
-        Hello! I'm a passionate front-end developer with a focus on creating
-        user-friendly and interactive web applications.
-      </p>
-    </section>
+  <a href="#AboutMe" id='FirstLink'>About</a>
+
+  <div id='AboutMe'>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" alt="HTML5" class='Skill' />
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" alt="CSS3" class='Skill' />
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" alt="JavaScript" class='Skill' />
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" alt="React" class='Skill' />
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="GitHub" class='Skill' />
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" alt="npm" class='Skill'/>
+    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/webpack/webpack-plain.svg" alt="Webpack" class='Skill'/>
+  </div>
+</section>
+
   );
 };
 
